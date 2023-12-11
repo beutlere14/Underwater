883719ca649d0de781f1692ceb8653e4e3d12775
Eli Beutler <elibeutler@gmail.com>
2023-12-11 15:58:52 -0700

    Movement and Beginning Harpoon
    Harpoon still needs a lot more work as does vertical movement

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -14,20 +14,14 @@ public class GameManager : MonoBehaviour
 
     public List<PlayerController> players;
 
-    public static bool inStartingArea;
 
     public bool multiplayer;
     //Prefabs
     public GameObject playerControllerPrefab;
-    public GameObject playerTwoControllerPrefab;
-    public GameObject tankPawnPrefab;
+    public GameObject scubaPawnPrefab;
     public Transform playerSpawnTransform;
-    public Transform playerTwoSpawnTransform;
-    public GameObject mapSpawner;
 
-    //multiplayer objects
-    public GameObject tankPawnPlayerMultiPrefab;
-    public GameObject tankPawnPlayerMultiTwoPrefab;
+
 
     //For High Score, Current score, and number of lives
     public float highScore = 0;
@@ -67,17 +61,11 @@ public class GameManager : MonoBehaviour
               
                     SpawnPlayer();
 
-                if (multiplayer == true)
-                {
-                    SpawnPlayerTwo();
-                }
+              
 
             }
         }
-        if (killScore > highScore)
-        {
-            highScore = killScore;
-        }
+       
     }
 
 
@@ -89,7 +77,7 @@ public class GameManager : MonoBehaviour
             GameObject newPlayerObj = Instantiate(playerControllerPrefab, Vector3.zero, Quaternion.identity);
 
             // Spawn the pawn and connect it to the controller
-            GameObject newPawnObj = Instantiate(tankPawnPrefab, playerSpawnTransform.position, playerSpawnTransform.rotation);
+            GameObject newPawnObj = Instantiate(scubaPawnPrefab, playerSpawnTransform.position, playerSpawnTransform.rotation);
 
             //Get the PlayerController component and Pawn component
             Controller newController = newPlayerObj.GetComponent<Controller>();
@@ -100,95 +88,11 @@ public class GameManager : MonoBehaviour
         }
 
 
-        if (multiplayer == true)
-        {
-            // Spawn player controller at (0,0,0) with no rotation
-            GameObject newPlayerObj = Instantiate(playerControllerPrefab, Vector3.zero, Quaternion.identity);
-
-            // Spawn the pawn and connect it to the controller
-            GameObject newPawnObj = Instantiate(tankPawnPlayerMultiPrefab, playerSpawnTransform.position, playerSpawnTransform.rotation);
-
-            //Get the PlayerController component and Pawn component
-            Controller newController = newPlayerObj.GetComponent<Controller>();
-            Pawn newPawn = newPawnObj.GetComponent<Pawn>();
-
-            // Hook them up
-            newController.pawn = newPawn;
-
-            this.lives = 6;
-        }
-
-
-
-    }
-
-    public void SpawnPlayerTwo()
-    {
-        // Spawn player controller at (0,0,0) with no rotation
-        GameObject newPlayerObj = Instantiate(playerTwoControllerPrefab, Vector3.zero, Quaternion.identity);
-
-        // Spawn the pawn and connect it to the controller
-        GameObject newPawnObj = Instantiate(tankPawnPlayerMultiTwoPrefab, playerTwoSpawnTransform.position, playerTwoSpawnTransform.rotation);
-
-        //Get the PlayerController component and Pawn component
-        Controller newController = newPlayerObj.GetComponent<Controller>();
-        Pawn newPawn = newPawnObj.GetComponent<Pawn>();
-
-        // Hook them up
-        newController.pawn = newPawn;
-        if (newPlayerObj != null)
-        {
-            if (GetComponent<Camera>() != null)
-            {
-                newPawnObj.GetComponent<Camera>().depth = 0;
-            }
-        }
-
-        lives = 6;
-
-
-    }
-    public void reload()
-    {
-        //The old method of reload
-        //   if (mapSpawner != null)
-        // {
-
-        // Instantiate(mapSpawner, Vector3.zero, Quaternion.identity);       
-        //}
-
-        SpawnPlayer();
 
     }
 
 
 
 
-
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            inStartingArea = false;
-           
-
-
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        { 
-        inStartingArea = true;
-
-        }
-    }
-
-    public void destroyGameManager()
-    {
-        Destroy(gameObject);
-    }
 
 }
